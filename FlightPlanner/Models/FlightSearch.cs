dfb5dcd5cb4373b9ebb463b539d7e5c4bad8814c
Niklavs Kalvans <niklavskalvans@gmail.com>
2021-10-20 17:53:44 +0300

    1st itteration fixed

--- FlightPlanner/Models/FlightSearch.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FlightPlanner.Models
-{
-    public class FlightSearch
-    {
-        public string from { get; set; }
-        public string to { get; set; }
-        public string departureDate { get; set; }
-    }
-}
---
+++ Flight_planner_1st/FlightPlanner/Models/FlightSearch.cs
@@ -0,0 +1,10 @@
+﻿
+namespace FlightPlanner.Models
+{
+    public class FlightSearch
+    {
+        public string from { get; set; }
+        public string to { get; set; }
+        public string departureDate { get; set; }
+    }
+}
